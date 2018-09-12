efe398e5af7499517e1b0c52aa6b8f2fd6e271c3
unknown <ajameslusk@gmail.com>
2018-09-12 15:09:47 -0400

    created fake data creating utility functions, fake data, schemas, and applicable root queries for users, addresses, DOBs, coordinates, posts, and comments

---
+++ utils/posts/create-posts.js
@@ -0,0 +1,39 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const existingUsers = 25;
+const postsToCreate = 75;
+let posts = [];
+const commentsToCreate = 225;
+let comments = [];
+
+for(let i = 1; i <= postsToCreate; i++){
+  let post = {};
+  post.id = i;
+  post.userId = Math.floor((Math.random()*existingUsers)+1);
+  post.title = faker.lorem.sentence(Math.floor(Math.random()*8)+1);
+  post.body = faker.lorem.paragraphs(Math.floor(Math.random()*3)+1);
+  post.timestamp = Date.now() - Math.floor(Math.random()*31536000000);
+  post.likes = Math.floor((Math.random()*(existingUsers/2))+1);
+  posts.push(post);
+}
+
+for(let i = 1; i <= commentsToCreate; i++){
+  let comment = {};
+  comment.id = i;
+  comment.postId = Math.floor((Math.random()*postsToCreate)+1);
+  comment.userId = Math.floor((Math.random()*existingUsers)+1);
+  comment.body = faker.lorem.sentences(Math.floor(Math.random()*3)+1);
+  comment.timestamp = posts[comment.postId-1].timestamp + Math.floor(Math.random()*(Date.now()-posts[comment.postId-1].timestamp));
+  comment.likes = Math.floor((Math.random()*(existingUsers/3))+1);
+  comments.push(comment);
+}
+
+// console.log(posts);
+// console.log(comments);
+
+const postsData = JSON.stringify(posts, null, 2);
+fs.writeFile('posts.json', postsData, ()=>{console.log('wrote to posts.json');});
+
+const commentsData = JSON.stringify(comments, null, 2);
+fs.writeFile('comments.json', commentsData, ()=>{console.log('wrote to comments.json');});
---
+++ utils/user/user-helpers.js
@@ -0,0 +1,94 @@
+
+const createFakeEmail = (firstName, lastName) => {
+  let email = '';
+  let number = Math.floor(Math.random()*10);
+  switch(number){
+  case(1):
+    email = firstName + '' + lastName + '@gmail.com';
+    break;
+  case(2):
+    email = firstName[0] + '' + lastName + '@gmail.com';
+    break;
+  case(3):
+    email = firstName + '' + lastName[0] + '@gmail.com';
+    break;
+  case(4):
+    email = firstName + '_' + lastName + '@gmail.com';
+    break;
+  case(5):
+    email = firstName + '.' + lastName + '@yahoo.com';
+    break;
+  case(6):
+    email = firstName[0] + '' + lastName + '@yahoo.com';
+    break;
+  case(7):
+    email = firstName[0] + '' + lastName + '@outlook.com';
+    break;
+  case(8):
+    email = firstName + '.' + lastName + '@outlook.com';
+    break;
+  case(9):
+    email = firstName + `@${lastName}.com`;
+    break;
+  default:
+    email = firstName + `@${lastName}.me`;
+  }
+  return email;
+};
+
+const createFakeUsername = (firstName, lastName, DOB) => {
+  let username = '';
+  let number = Math.floor(Math.random()*10);
+  switch(number){
+  case(1):
+    username = firstName + '' + lastName;
+    break;
+  case(2):
+    username = firstName[0] + '' + lastName;
+    break;
+  case(3):
+    username = firstName + '' + lastName[0];
+    break;
+  case(4):
+    username = firstName + lastName + DOB.year;
+    break;
+  case(5):
+    username = firstName + lastName[0] + lastName[1];
+    break;
+  case(6):
+    username = 'real' + firstName + lastName;
+    break;
+  case(7):
+    username = firstName + 'lovesHorses';
+    break;
+  case(8):
+    username = 'Official' + lastName + firstName;
+    break;
+  case(9):
+    username = lastName + firstName;
+    break;
+  default:
+    username = firstName + '2';
+  }
+
+  if(Math.random() > 0.25 || username.length < 9){
+    return username + Math.floor(Math.random()*100);
+  } else {
+    return username;
+  }
+};
+
+const createFakeDOB = () => {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  const DOB = {};
+  DOB.month = months[Math.floor(Math.random()*12)];
+  DOB.day = Math.floor(Math.random()*28); // At least 28 days in each month, saves writing more logic
+  DOB.year = 1950 + Math.floor((Math.random()*50)+(Math.random()*50)); // Ensures all users are at least 13 and tend to b twenties or thirties
+  if(DOB.year > 2004){
+    DOB.year = 2004 - Math.floor(Math.random()*6);
+  }
+  return DOB;
+};
+
+module.exports = {createFakeEmail, createFakeUsername, createFakeDOB};
